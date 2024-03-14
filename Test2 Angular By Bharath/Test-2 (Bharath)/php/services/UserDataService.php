4402492704c3c5d2fca9cf4d60028fb0261596cc
Bharath <mesani.bkr@gmail.com>
2024-03-14 18:53:58 +0530

    Adding angular test2

---
+++ Test2 Angular By Bharath/Test-2 (Bharath)/php/services/UserDataService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+class UserDataService{
+    public static function userData(){
+
+        $token = $_POST['token'];
+        $query = "SELECT * FROM users WHERE token = :token";
+        $user = DB->first($query, ["token" => $token]);
+        if (!$user) {
+            return response(["status"=>"false","message" => "Something went wrong!"], 400);
+        }
+        return response(["status"=>"true","message" => "Login Success", "data"=>["userdata" => $user]], 200);
+    }
+
+
+
+
+
+}
+
+
